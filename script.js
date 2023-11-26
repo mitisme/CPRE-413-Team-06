0e25e3d126b355bd86b99c6f28af05b99caddba0
Michael <111mitisme@gmail.com>
2023-11-26 15:44:52 -0600

    added a button that on button press takes your lat and long and returns know location

---
+++ script.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const getLocationButton = document.getElementById('get-location');
+    const locationDataDiv = document.getElementById('location-data');
+
+    getLocationButton.addEventListener('click', function () {
+        if ('geolocation' in navigator) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                const latitude = position.coords.latitude;
+                const longitude = position.coords.longitude;
+
+                // Use OpenCage Geocoder API to get address
+                const apiKey = '1dce474fdd5346eda8a730295ddf92e1'; // Replace with your actual OpenCage Geocoder API key
+                const geocodingApiUrl = `https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=${apiKey}`;
+
+                fetch(geocodingApiUrl)
+                    .then(response => response.json())
+                    .then(data => {
+                        if (data.results && data.results.length > 0) {
+                            const address = data.results[0].formatted;
+                            locationDataDiv.innerHTML = `Location Address: ${address}`;
+                        } else {
+                            locationDataDiv.innerHTML = 'Unable to retrieve address for the provided coordinates.';
+                        }
+                    })
+                    .catch(error => {
+                        locationDataDiv.innerHTML = `Error: ${error.message}`;
+                    });
+            }, function (error) {
+                locationDataDiv.innerHTML = `Error: ${error.message}`;
+            });
+        } else {
+            locationDataDiv.innerHTML = 'Geolocation is not available in your browser.';
+        }
+    });
+});
