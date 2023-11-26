4218182215dcec78f01321c4264d6f5db730b996
Michael <111mitisme@gmail.com>
2023-11-26 15:53:27 -0600

    added addtional IP information that can be gathered

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 const longitude = position.coords.longitude;
 
                 // Use OpenCage Geocoder API to get address
-                const apiKey = '1dce474fdd5346eda8a730295ddf92e1'; // Replace with your actual OpenCage Geocoder API key
+                const apiKey = '1dce474fdd5346eda8a730295ddf92e1'; // Replace with your actual Geocoder API key
                 const geocodingApiUrl = `https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=${apiKey}`;
 
                 fetch(geocodingApiUrl)
@@ -36,12 +36,25 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     getIPButton.addEventListener('click', function () {
-        // Use a simple HTTP request to get the user's IP address from an IP lookup service
-        fetch('https://ipapi.co/json/')
+        // Use the IPinfo API to get more information about the user's IP address
+        fetch('https://ipinfo.io/json')
             .then(response => response.json())
             .then(data => {
                 const ipAddress = data.ip;
-                ipDataDiv.innerHTML = `Your IP Address: ${ipAddress}`;
+                const hostname = data.hostname;
+                const city = data.city;
+                const region = data.region;
+                const country = data.country;
+                const isp = data.org;
+
+                ipDataDiv.innerHTML = `
+                    IP Address: ${ipAddress}<br>
+                    Hostname: ${hostname}<br>
+                    City: ${city}<br>
+                    Region: ${region}<br>
+                    Country: ${country}<br>
+                    ISP: ${isp}
+                `;
             })
             .catch(error => {
                 ipDataDiv.innerHTML = `Error: ${error.message}`;
